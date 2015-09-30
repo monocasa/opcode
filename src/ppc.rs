10b9f45f3baf00a11d06d39690241b23fa36d215
monocasa <tristan.hastings.miller@gmail.com>
2015-09-29 21:24:41 -0600

    Add decoding for lbzx

--- src/ppc.rs
+++ src/ppc.rs
@@ -28,11 +28,13 @@ pub enum Mne {
 	Addi,
 	Addis,
 	Lbz,
+	Lbzx,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Op {
 	RtDRa(Mne, Reg, i16, Reg),
+	RtRaRb(Mne, Reg, Reg, Reg),
 }
 
 fn opcd(instr: u32) -> u16 {
@@ -51,9 +53,37 @@ fn d_d(instr: u32) -> i16 {
 	(instr & 0xFFFF) as i16
 }
 
+fn x_ra(instr: u32) -> Reg {
+	Reg::Gpr(((instr >> 16) & 0x1F) as u8)
+}
+
+fn x_rb(instr: u32) -> Reg {
+	Reg::Gpr(((instr >> 11) & 0x1F) as u8)
+}
+
+fn x_rt(instr: u32) -> Reg {
+	Reg::Gpr(((instr >> 21) & 0x1F) as u8)
+}
+
+fn x_xo(instr: u32) -> u16 {
+	((instr >> 1) & 0x3FF) as u16
+}
+
+#[allow(unused)]
+fn decode_special(instr: u32, addr: Addr, uarch: Uarch) -> Result<Op, DisError> {
+	let op = match x_xo(instr) {
+		87 => Op::RtRaRb(Mne::Lbzx, x_rt(instr), x_ra(instr), x_rb(instr)),
+
+		_ => return Err(DisError::Unknown{num_bytes: 4}),
+	};
+
+	Ok(op)
+}
+
 #[allow(unused)]
 pub fn decode(instr: u32, addr: Addr, uarch: Uarch) -> Result<Op, DisError> {
 	let op = match opcd(instr) {
+		31 => return decode_special(instr, addr, uarch),
 		34 => Op::RtDRa(Mne::Lbz, d_rt(instr), d_d(instr), d_ra(instr)),
 		_ => return Err(DisError::Unknown{num_bytes: 4}),
 	};
@@ -97,12 +127,20 @@ impl Disassembler for PpcDis {
 mod tests {
 	use super::*;
 
+	fn decode_i(instr: u32) -> Op {
+		decode(instr, 0, Uarch::Ppc206B).unwrap()
+	}
+
 	#[test]
 	fn decode_lbz() {
-		assert_eq!(decode(0x89230080, 0, Uarch::Ppc206B).unwrap(),
-			Op::RtDRa(Mne::Lbz, Reg::Gpr(9),    128, Reg::Gpr(3)));
-		assert_eq!(decode(0x88EAB004, 0, Uarch::Ppc206B).unwrap(),
-			Op::RtDRa(Mne::Lbz, Reg::Gpr(7), -20476, Reg::Gpr(10)));
+		assert_eq!(decode_i(0x89230080), Op::RtDRa(Mne::Lbz, Reg::Gpr(9),    128, Reg::Gpr( 3)));
+		assert_eq!(decode_i(0x88EAB004), Op::RtDRa(Mne::Lbz, Reg::Gpr(7), -20476, Reg::Gpr(10)));
+	}
+
+	#[test]
+	fn decode_lbzx() {
+		assert_eq!(decode_i(0x7C7F50AE), Op::RtRaRb(Mne::Lbzx, Reg::Gpr( 3), Reg::Gpr(31), Reg::Gpr(10)));
+		assert_eq!(decode_i(0x7D4340AE), Op::RtRaRb(Mne::Lbzx, Reg::Gpr(10), Reg::Gpr( 3), Reg::Gpr( 8)));
 	}
 }
 
