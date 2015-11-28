29dc5426f9a44a28c87f959b4240b2591941b315
monocasa <tristan.hastings.miller@gmail.com>
2015-11-28 15:56:08 -0700

    FEATURE:  Stubbed out mips::MipsDisasm

--- src/mips.rs
+++ src/mips.rs
@@ -1,4 +1,4 @@
-use super::Addr;
+use super::{Addr, Disassembler, DisError, DisResult};
 
 pub enum Uarch {
 	LsiR2000,       // Canonical MIPS-I processor
@@ -28,7 +28,106 @@ pub fn uarch_info_for_uarch(uarch: Uarch) -> &'static UarchInfo {
 	}
 }
 
+pub enum Mne {
+	Add,
+	Addi,
+	Addiu,
+	Addu,
+	And,
+	Andi,
+	Bczt,
+	Bczf,
+	Beq,
+	Bgez,
+	Bgezal,
+	Bgtz,
+	Blez,
+	Bltz,
+	Bltzal,
+	Bne,
+	Break,
+	Cfcz,
+	Copz,
+	Ctcz,
+	Div,
+	Divu,
+	J,
+	Jal,
+	Jalr,
+	Jr,
+	Lb,
+	Lbu,
+	Lh,
+	Lhu,
+	Ll,
+	Lui,
+	Lw,
+	Lwcz,
+	Lwl,
+	Lwr,
+	Mfcz,
+	Mfhi,
+	Mflo,
+	Mtcz,
+	Mthi,
+	Mtlo,
+	Mult,
+	Multu,
+	Nor,
+	Or,
+	Ori,
+	Rfe,
+	Sb,
+	Sc,
+	Sh,
+	Sll,
+	Sllv,
+	Slt,
+	Slti,
+	Sltiu,
+	Sltu,
+	Sra,
+	Srl,
+	Srlv,
+	Srav,
+	Sub,
+	Subu,
+	Sw,
+	Swcz,
+	Swl,
+	Syscall,
+	Tlbr,
+	Tlbp,
+	Tlbwi,
+	Tlbwr,
+	Wait,
+	Swr,
+	Xor,
+	Xori,
+}
+
 #[allow(unused_variables)]
 pub fn decode_instr(instr: u32, addr: Addr, uarch_info: &UarchInfo) {
+}
+
+pub struct MipsDisasm;
+
+impl Disassembler for MipsDisasm {
+	#[allow(unused_variables)]
+	fn disassemble(&self, addr: Addr, buf: &[u8]) -> DisResult  {
+		if buf.len() < 4 {
+			return Err(DisError::MemOverflow);
+		}
+
+		if (addr % 4) != 0 {
+			return Err( DisError::Unaligned{ desired_alignment: 4, } );
+		}
+
+		Err(DisError::Unknown{num_bytes: 4})
+	}
+
+	fn bytes_per_unit(&self) -> u16 {
+		4
+	}
 }
 
